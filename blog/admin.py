3a97b0163d463a5cf3032a5c2d77d8ed1d5ca2d7
park31821 <74476122+park31821@users.noreply.github.com>
2025-09-21 17:41:46 +1000

    feat: add filters

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "created_at"]
+    list_filter = ["created_at", "author"]
+    search_fields = ["title", "content"]
+
+
+admin.site.register(Post, PostAdmin)
