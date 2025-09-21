c9ca583905749e6cd20e5aaa901122b2f52b9f8c
park31821 <74476122+park31821@users.noreply.github.com>
2025-09-21 17:28:23 +1000

    feat: add author field

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200, default="Unknown Author")
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
